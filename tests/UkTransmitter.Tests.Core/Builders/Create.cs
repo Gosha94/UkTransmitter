3d798558710067832620bd833118dcf96ab5d32d
Gosha94 <gerizch@rambler.ru>
2022-02-06 02:06:05 +0300

    add test builder

---
+++ tests/UkTransmitter.Tests.Core/Builders/Create.cs
@@ -0,0 +1,17 @@
+﻿namespace UkTransmitter.Tests.Core.Builders
+{
+    internal class Create
+    {
+
+        #region Workflows
+
+        internal static void NewTestWorkflow() => new string(new char[] { 'a', 'b', 'c' });
+
+        #endregion
+
+        #region Services
+
+        #endregion
+
+    }
+}
